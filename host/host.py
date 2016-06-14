d97bdac5b4a29a0af79e4712465ab2829cc57e62
ducky <richard.lin@berkeley.edu>
2016-06-13 17:35:41 -0700

    Refactor host

--- host/host.py
+++ host/host.py
@@ -9,10 +9,10 @@ from duckypacket import *
 
 CHUNK_SIZE = 128
 
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 parser = argparse.ArgumentParser(description='Bootloader host')
-parser.add_argument('bin_file', type=file,
+parser.add_argument('bin_file', type=str,
                     help='bin file to load')
 parser.add_argument('serial', type=str,
                     help='serial port to use, like COM1 (Windows) or /dev/ttyACM0 (Linux)')
@@ -22,7 +22,7 @@ parser.add_argument('--address', type=int, default=0x8000000 + 16*1024,
                     help='starting address')
 parser.add_argument('--memsize', type=int, default=48*1024,
                     help='memory size')
-parser.add_argument('--device', type=int, default=1,
+parser.add_argument('--devices', type=int, nargs='+',
                     help='device number, 0 is master, slaves start at 1')
 
 args = parser.parse_args()
@@ -30,54 +30,75 @@ args = parser.parse_args()
 ser = serial.Serial(args.serial, args.baud)
 logging.info("Opened serial port '%s'", args.serial)
 
-# flush the receive buffer
-time.sleep(0.1) # wait for some time to initialize the serial object, otherwise the initial flush doesn't work
-bytes_read = ser.read(ser.inWaiting())
-logging.debug("Serial: flushed %i bytes", len(bytes_read))
+# General exception when the bootloader returns a non-success error code.
+class BootloaderResponseError(Exception):
+  pass
 
-curr_address = args.address
+class BootloaderComms(object):
+  def __init__(self, ser):
+    self.serial = ser
+    self.serial.write(b'\x00')
 
-logging.debug("Serial -> Erase(0x% 8x, %i)", args.address, args.memsize)
+  def command(self, packet, reply_expected=True):
+    self.serial.write(cobs_encode(packet.get_bytes()) + b'\x00')
+    if reply_expected:
+      line = ser.readline().strip()
+      logging.debug("Serial <- '%s'", line)  # discard the ending space
+      if (line != 'D'):
+        raise BootloaderResponseError("Got '%s' error code from bootloader" % line)
 
-packet = PacketBuilder()
-packet.put_uint8(ord('E'))
-packet.put_uint8(args.device)
-packet.put_uint32(args.address)
-packet.put_uint32(args.memsize)
-ser.write(b'\x00' + cobs_encode(packet.get_bytes()) + b'\x00')
-line = ser.readline()
+  def erase(self, device, address, length):
+    packet = PacketBuilder()
+    packet.put_uint8(ord('E'))
+    packet.put_uint8(device)
+    packet.put_uint32(address)
+    packet.put_uint32(length)
+    self.command(packet)
 
-logging.debug("Serial <- '%s'", line.strip())  # discard the ending space
-
-while True:
-  chunk = args.bin_file.read(CHUNK_SIZE)
-  if chunk:
-    logging.debug("Serial -> Write(0x% 8x -> %i)", curr_address, len(chunk))
-
+  def write(self, device, address, data):
     packet = PacketBuilder()
     packet.put_uint8(ord('W'))
-    packet.put_uint8(args.device)
-    packet.put_uint32(curr_address)
-    packet.put_uint32(binascii.crc32(chunk) & 0xffffffff)
-    packet.put_bytes(chunk, len(chunk))
-    ser.write(cobs_encode(packet.get_bytes()) + b'\x00')
+    packet.put_uint8(device)
+    packet.put_uint32(address)
+    packet.put_uint32(binascii.crc32(data) & 0xffffffff)
+    packet.put_bytes(data, len(data))
+    self.command(packet)
 
-    line = ser.readline().strip()
-    logging.debug("Serial <- '%s'", line)  # discard the ending space
-    if (line != 'D'):
-      logging.error("Bad response from bootloader")
-      exit()
+  def run_app(self, device, address):
+    packet = PacketBuilder()
+    packet.put_uint8(ord('J'))
+    packet.put_uint8(device)
+    packet.put_uint32(address)
+    self.command(packet, reply_expected=False)
 
-    curr_address += CHUNK_SIZE
-  else:
-    break
+  def program_and_run(self, device, memsize, address, program_bin):
+    time.sleep(0.1) # wait for some time to initialize the serial object, otherwise the initial flush doesn't work
+    bytes_read = ser.read(ser.inWaiting())
+    logging.debug("Serial: flushed %i bytes", len(bytes_read))
 
-logging.debug("Serial -> Jump(0x% 8x)" % args.address)
+    self.erase(device, address, memsize)
+    logging.info("Erased %i bytes from device %i", memsize, device)
+    curr_address = address
 
-packet = PacketBuilder()
-packet.put_uint8(ord('J'))
-packet.put_uint8(args.device)
-packet.put_uint32(args.address)
-ser.write(cobs_encode(packet.get_bytes()) + b'\x00')
+    while True:
+      chunk = program_bin.read(CHUNK_SIZE)
+      if chunk:
+        self.write(device, curr_address, chunk)
+        curr_address += len(chunk)
+      else:
+        break
+    logging.info("Write %i bytes to device %i", curr_address - address, device)
+
+    self.run_app(device, address)
+    logging.info("Started app on device %i", device)
+
+bootloader = BootloaderComms(ser)
+
+# TODO: ensure device 0 isn't programmed except at end
+
+for device in args.devices:
+  bin_file = open(args.bin_file, 'r')
+  bootloader.program_and_run(device, args.memsize, args.address, bin_file)
+  bin_file.close()
 
 ser.close()
